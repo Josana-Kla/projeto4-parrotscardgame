f2b77803d34c6645fe192cd8f078c773c80bfa92
JosanaKla <josana.0205@gmail.com>
2022-06-16 16:04:38 -0300

    feat: added select and rotation

--- scripts/script.js
+++ scripts/script.js
@@ -2,23 +2,35 @@ let numberOfCards = prompt("Com quantas cartas você quer jogar?");
 let numberOfCardsChosen = []; 
 
 let card = `
-  <ul>
-    <div class="each-card">
-      <img src="./assets/front.png"/>
-    </div>
-  </ul>
+  <div class="each-card" onclick="showCardImage(this)">
+    <img src="./assets/front.png"/>
+  </div>
 `;
 
 function askForNumberOfCards() {
 
   for(let i = 1; i <= numberOfCards; i++) {
-    if(numberOfCards >= 4 && numberOfCards <= 14) {
-      if(numberOfCards % 2 === 0) {
-        document.querySelector(".cards").innerHTML += card;
-        numberOfCards-1;
-      }
-    }
+    if(numberOfCards >= 4 && numberOfCards <= 14 && numberOfCards % 2 === 0) {
+      document.querySelector(".cards").innerHTML += card;
+      numberOfCards-1;
+    } /* else {
+      numberOfCards = prompt("Escolha um número PAR entre 4 e 14");
+      document.querySelector(".cards").innerHTML += card;
+      numberOfCards+1;
+    } */
   }
 }
 
-askForCards()
+askForNumberOfCards()
+
+function showCardImage(element) {
+  let cardClicked = document.querySelector(".selectAndRotation");
+
+  if(cardClicked !== null) {
+    cardClicked.classList.remove("selectAndRotation");
+  }
+
+  element.classList.add("selectAndRotation");
+}
+
+showCardImage()
